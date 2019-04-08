648b207ea2d76df3350e5fa17eb1f25ccbb2c128
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-04-08 04:52:17 -0500

    Added address display

---
+++ src/components/AddressDisplay.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>{{ address }}</div>
+</template>
+
+<script>
+export default {
+	name: 'AddressDisplay',
+	props: {
+		address: {
+			type: String,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Address.vue
+++ src/views/Address.vue
@@ -1,17 +1,20 @@
 <template>
 	<div>
+		<AddressDisplay :address="address"/>
 		<QRCode :address="address"/>
 		<CopyAddressButton :address="address"/>
 	</div>
 </template>
 
 <script>
+import AddressDisplay from '@/components/AddressDisplay.vue';
 import QRCode from '@/components/QRCode.vue';
 import CopyAddressButton from '@/components/CopyAddressButton.vue';
 
 export default {
 	name: 'Address',
 	components: {
+		AddressDisplay,
 		QRCode,
 		CopyAddressButton,
 	},

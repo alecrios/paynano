bdee8706298c8cecbfc4353a050c7ec4d1e0cb81
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-04-08 04:25:45 -0500

    Added address routing

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/views/Home.vue';
+import Address from '@/views/Address.vue';
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
 			path: '/',
 			name: 'Home',
 			component: Home,
+		},
+		{
+			path: '/:address',
+			name: 'Address',
+			component: Address,
 		},
 	],
 });
---
+++ src/views/Address.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		{{ address }}
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Address',
+	computed: {
+		address() {
+			return this.$route.params.address;
+		},
+	},
+};
+</script>
+
+<style scoped>
+
+</style>

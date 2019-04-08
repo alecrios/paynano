40c3539d4182d385aa0d937f23fbe678b8d8cee5
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-04-08 04:49:36 -0500

    Added copy address button

---
+++ src/components/CopyAddressButton.vue
@@ -0,0 +1,26 @@
+<template>
+	<button @click="copy">Copy Address</button>
+</template>
+
+<script>
+import copy from 'copy-to-clipboard';
+
+export default {
+	name: 'CopyAddressButton',
+	props: {
+		address: {
+			type: String,
+			required: true,
+		},
+	},
+	methods: {
+		copy() {
+			copy(this.address);
+		},
+	},
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Address.vue
+++ src/views/Address.vue
@@ -1,16 +1,19 @@
 <template>
 	<div>
 		<QRCode :address="address"/>
+		<CopyAddressButton :address="address"/>
 	</div>
 </template>
 
 <script>
 import QRCode from '@/components/QRCode.vue';
+import CopyAddressButton from '@/components/CopyAddressButton.vue';
 
 export default {
 	name: 'Address',
 	components: {
 		QRCode,
+		CopyAddressButton,
 	},
 	computed: {
 		address() {

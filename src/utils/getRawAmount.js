18edba560f4f203bb294ba9282f901c16b9a2f31
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-04-13 06:51:19 -0500

    Fixed issues with raw amount calculation

--- src/utils/getRawAmount.js
+++ src/utils/getRawAmount.js
@@ -1,9 +1,7 @@
-import scientificToDecimal from 'scientific-to-decimal';
+import BigNumber from 'bignumber.js';
 
 const RAW_PER_UNIT = 1000000000000000000000000000000;
 
 export default function getRawAmount(amount) {
-	const rawAmountScientific = parseFloat(amount) * RAW_PER_UNIT;
-	const rawAmount = scientificToDecimal(rawAmountScientific);
-	return rawAmount;
+	return new BigNumber(parseFloat(amount)).times(RAW_PER_UNIT).toFixed(0);
 }

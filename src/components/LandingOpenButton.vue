0808e2e25ec6e410f7cf7ffadc289fab9ac528ee
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-04-13 06:09:20 -0500

    Added note about wallet compatibility

--- src/components/LandingOpenButton.vue
+++ src/components/LandingOpenButton.vue
@@ -5,6 +5,8 @@
 			v-text="'Open in Wallet'"
 			:href="deepLink"
 		/>
+
+		<div class="note">* Not supported by all wallets</div>
 	</div>
 </template>
 
@@ -23,9 +25,18 @@ export default {
 <style lang="scss" scoped>
 .landing-open-button {
 	padding: 1.5rem;
-}
 
-.button {
-	@include button;
+	.button {
+		@include button;
+	}
+
+	.note {
+		margin-top: .5rem;
+		text-align: center;
+		font-size: .75rem;
+		line-height: .75rem;
+		font-style: italic;
+		color: $color-text-lighter;
+	}
 }
 </style>

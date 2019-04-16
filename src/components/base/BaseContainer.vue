cb540a094f505e31eb1570be59ec1250b991aa2b
Alec Rios <8664387+alecrios@users.noreply.github.com>
2019-04-15 23:20:58 -0500

    Added base container

---
+++ src/components/base/BaseContainer.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="container">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BaseContainer',
+};
+</script>
+
+<style lang="scss">
+.container {
+	border-radius: .5rem;
+	overflow: hidden;
+	background-color: $color-base;
+	box-shadow: $box-shadow-large;
+}
+</style>
